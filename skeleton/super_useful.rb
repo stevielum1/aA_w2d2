36d842c5b49102ba2416816b6e7e02955bf22543
Steven Lum <stevielum1@gmail.com>
2018-06-25 14:21:53 -0700

    Second commit

--- skeleton/super_useful.rb
+++ skeleton/super_useful.rb
@@ -1,7 +1,11 @@
 # PHASE 2
 def convert_to_int(str)
   Integer(str)
+rescue ArgumentError => e
+  e.message
+  return nil
 end
+
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
@@ -9,8 +13,10 @@ FRUITS = ["apple", "banana", "orange"]
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError
   else 
-    raise StandardError 
+    raise FruitError 
   end 
 end
 
@@ -20,13 +26,32 @@ def feed_me_a_fruit
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
   reaction(maybe_fruit) 
+rescue CoffeeError => e
+  p e.message
+  retry
+rescue FruitError => e
+  p e.message
 end  
+
+class FruitError < StandardError
+  def message
+    "Not a fruit"
+  end
+end
+
+class CoffeeError < StandardError
+  def message
+    "Coffe is not a fruit"
+  end
+end
 
 # PHASE 4
 class BestFriend
-  def initialize(name, yrs_known, fav_pastime)
+  def initialize(name, yrs_known, fav_pastime = nil)
+    raise StandardError if yrs_known < 5
     @name = name
     @yrs_known = yrs_known
+    raise DescriptiveError if fav_pastime.nil?
     @fav_pastime = fav_pastime
   end
 
@@ -43,4 +68,8 @@ class BestFriend
   end
 end
 
-
+class DescriptiveError < StandardError
+  def message
+    "Description is error"
+  end
+end
